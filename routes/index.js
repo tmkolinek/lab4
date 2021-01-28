67cae79812d7aa6651d03964abcbda066e865d5d
tmkolinek <77120701+tmkolinek@users.noreply.github.com>
2021-01-28 10:58:21 -0800

    finished

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+  	"name": "Waiting in Line",
+  	"image": "lorempixel.people.1.jpeg",
+  	"id":"project1"
+  });
 };
